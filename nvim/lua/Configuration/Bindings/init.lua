d0a4d0c019ef6c3cd847e6233e3a421a1c59229e
Eralp Çelebi <eralp.celebi.personal@gmail.com>
2025-06-21 09:36:03 +0200

    Fixed keybindings.

--- nvim/lua/Configuration/Bindings/init.lua
+++ nvim/lua/Configuration/Bindings/init.lua
@@ -4,32 +4,29 @@ vim.g.maplocalleader = "§"
 
 local set = vim.keymap.set
 local cmd = vim.cmd
-local builtin = require('telescope.builtin')
 
-
-set('n', '<leader>n', function() vim.cmd("Oil") end, { desc = "Open file tree." })
-set('n', '<leader>FF', builtin.find_files, { desc = 'Telescope find files' })
-set('n', '<leader>FG', builtin.live_grep, { desc = 'Telescope live grep' })
-set('n', '<leader>FB', builtin.buffers, { desc = 'Telescope buffers' })
-set('n', '<leader>FH', builtin.help_tags, { desc = 'Telescope help tags' })
-set('n', '<leader>gs', builtin.git_status, { desc = 'Bring up git status.' })
-set('n', '<leader>gb', builtin.git_branches, { desc = 'Bring up git branches.' })
-set('n', '<leader>gc', builtin.git_commits, { desc = 'Bring up git commits.' })
+set('n', '<leader>n', function() vim.cmd("Oil") end, { desc = "Open file tree. (Oil)" })
+set('n', '<leader>FF', builtin.find_files, { desc =  "Find files. (Telescope)"})
+set('n', '<leader>FG', builtin.live_grep, { desc = "Start live grep. (Telescope)" })
+set('n', '<leader>FB', builtin.buffers, { desc = "List buffers. (Telescope)" })
+set('n', '<leader>FH', builtin.help_tags, { desc = "Search help tags. (Telescope)" })
+set('n', '<leader>gs', builtin.git_status, { desc = "Get git status. (Telescope)" })
+set('n', '<leader>gb', builtin.git_branches, { desc = "Get git branches. (Telescope)" })
+set('n', '<leader>gc', builtin.git_commits, { desc = "Get git commits. (Telescope)" })
 
 vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function()
-      local bufopts = { noremap=true, silent=true }
-      set('n', '<leader>de', builtin.lsp_type_definitions, bufopts)
-      set('n', '<leader>df', builtin.lsp_definitions, bufopts)
-      set('n', '<leader>D', builtin.diagnostics, bufopts)
-      set('n', '<leader>i', builtin.lsp_implementations, bufopts)
-      set('n', '<leader>rf', builtin.lsp_references, bufopts)
-      set('n', '<leader>s', vim.lsp.buf.signature_help, bufopts)
-      set('n', '<leader>h', vim.lsp.buf.hover, bufopts)
-      set('n', '<leader>tf', vim.lsp.buf.type_definition, bufopts)
-      set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
-      set('n', '<leader>a', vim.lsp.buf.code_action, bufopts)
-     
+  callback = function(args)
+    local builtin = require('telescope.builtin')
+    set('n', '<leader>de', builtin.lsp_type_definitions, { buffer = args.buf })
+    set('n', '<leader>df', builtin.lsp_definitions, { buffer = args.buf })
+    set('n', '<leader>D', builtin.diagnostics, { buffer = args.buf })
+    set('n', '<leader>i', builtin.lsp_implementations, { buffer = args.buf })
+    set('n', '<leader>rf', builtin.lsp_references, { buffer = args.buf })
+    set('n', '<leader>s', vim.lsp.buf.signature_help, { buffer = args.buf })
+    set('n', '<leader>h', vim.lsp.buf.hover, { buffer = args.buf })
+    set('n', '<leader>tf', vim.lsp.buf.type_definition, { buffer = args.buf })
+    set('n', '<leader>rn', vim.lsp.buf.rename, { buffer = args.buf })
+    set('n', '<leader>a', vim.lsp.buf.code_action, { buffer = args.buf })     
   end
 })
 
