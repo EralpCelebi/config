2a669e33f7a7a6fb29599c522cdae767ba788dcb
Eralp Çelebi <eralp.celebi.personal@gmail.com>
2025-06-21 09:59:36 +0200

    Added keybindings for 'trouble.nvim'

--- nvim/lua/Configuration/Packages/Specifications/trouble.lua
+++ nvim/lua/Configuration/Packages/Specifications/trouble.lua
@@ -6,27 +6,32 @@ return {
     {
       "<leader>xx",
       "<cmd>Trouble diagnostics toggle<cr>",
-      desc = "Diagnostics (Trouble)",
+      desc = "Diagnostics (trouble.nvim)",
     },
     {
-      "<leader>cs",
+      "<leader>xb",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (trouble.nvim)",
+    },
+    {
+      "<leader>xss",
       "<cmd>Trouble symbols toggle focus=false<cr>",
-      desc = "Symbols (Trouble)",
+      desc = "Symbols (trouble.nvim)",
     },
     {
-      "<leader>csl",
+      "<leader>xsr",
       "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-      desc = "LSP Definitions / references / ... (Trouble)",
+      desc = "LSP Definitions / references / ... (trouble.nvim)",
     },
     {
       "<leader>xl",
       "<cmd>Trouble loclist toggle<cr>",
-      desc = "Location List (Trouble)",
+      desc = "Location List (trouble.nvim)",
     },
     {
       "<leader>xq",
       "<cmd>Trouble qflist toggle<cr>",
-      desc = "Quickfix List (Trouble)",
+      desc = "Quickfix List (trouble.nvim)",
     },
   },
 }
